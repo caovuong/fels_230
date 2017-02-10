4f2a7f2b6c1e15b0b05a9a961d483a05da78cca6
BichThao <hoangthaomta5@gmail.com>
2017-02-10 12:55:22 +0700

    Task3 signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#show", page: "home"
   get "/pages/*page", to: "pages#show"
+  get "/signup", to: "users#new"
 
+  resources :users, only: [:create, :show]
   resources :categories, only: [:new, :create, :index]
 end
