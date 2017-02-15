6a34a37d22e3ce4eb24bc90aa7b9ff137fa2d69f
BichThao <hoangthaomta5@gmail.com>
2017-02-15 09:36:22 +0700

    show_update_delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :find_user, only: [:update, :edit, :show]
+  before_action :correct_user, only: [:edit, :update] 
+  
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def create
@@ -14,10 +22,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    unless @user
-      flash[:warning] = t("error.user")
-      redirect_to user_url
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "error.update_suc"
+      redirect_to @user
+    else
+      flash[:danger] = t "error.update_er"
+      render :edit
     end
   end
 
@@ -25,5 +41,18 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:warning] = t "error.user"
+      redirect_to users_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user == @user
   end
 end
