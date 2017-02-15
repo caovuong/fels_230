fc662166a72211e9f132bff36f2010434676a563
thaonguyen <tranthinguyen1994@gmail.com>
2017-02-15 20:03:57 +0700

    Add-word-and-answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207214226) do
+ActiveRecord::Schema.define(version: 20170214015311) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
